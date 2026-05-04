600c75726f63a8d9b589996ec09d152962e1ad18
Pranjal-Rathore49 <pranjalrathore852006@gmail.com>
2026-05-04 08:56:03 +0530

    billing system finalized

---
+++ Class1.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BillingSystemApp
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string ClientKaNaam { get; set; }
+    }
+
+    public enum Status { Draft, Done, PaisaMilGya }
+
+    public class Bill
+    {
+        public int Id { get; set; }
+        public int ClientId { get; set; }
+        public decimal TotalPaisa { get; set; }
+        public Status BillStatus { get; set; } = Status.Draft;
+        public void ChangeStatus(Status nayaStatus)
+        {
+            if (BillStatus == Status.Draft && nayaStatus == Status.Done)
+            {
+                BillStatus = Status.Done;
+            }
+            if (BillStatus == Status.Done && nayaStatus == Status.PaisaMilGya)
+            {
+                BillStatus = Status.PaisaMilGya;
+            }
+        }
+    }
+
+    public class MyDatabase : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Bill> Bills { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=BillingDB;Trusted_Connection=True;");
+        }
+    }
+}
---
+++ billing.cs
@@ -0,0 +1,40 @@
+﻿namespace BillingSystemApp
+{
+    public class HisabKitab
+    {
+        public decimal CalculateKrna(string type, decimal kitnaChala, decimal rate)
+        {
+            decimal result = 0;
+            if (type == "Flat")
+            {result = rate;
+            }
+            else if (type == "Usage")
+            {result = kitnaChala * rate;
+            }
+            else if (type == "Tire")
+            {
+                if (kitnaChala <= 100)
+                {result = kitnaChala * rate;
+                }
+                else
+                {result = (100 * rate) + ((kitnaChala - 100) * (rate - 2));
+                }
+            }
+            return result;
+        }
+
+        public decimal DiscountWala(decimal amnt, string discType, decimal val)
+        {
+            if (discType == "Sale")
+            {
+                decimal discount = (amnt * val) / 100;
+                return amnt - discount;
+            }
+            if (discType == "Coupon")
+            {
+                return amnt - val;
+            }
+            return amnt;
+        }
+    }
+}
